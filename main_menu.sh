56a80afb2b4a124bfca7e25a3c3dc5b9811ce2bf
sylviaRS <silvia.mark@gmail.com>
2012-07-14 08:03:59 +0000

    some_data

---
+++ main_menu.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+function main_menu()
+{
+
+echo -e -n "\n\nMAIN MENU\n\n : "
+
+while true; do
+        echo "1 AUTHENTICATE"
+        echo "2 DATABASE INSTANCES"
+        echo "3 EXIT"
+
+        read CONFIRM
+        case $CONFIRM in
+                1|PLEASEAUTHORISE)
+                        echo " PLEASE AUTHORISE: "
+                       if [ "~/rackspace_api_tool/authentication.sh" ]; then
+                                source ~/rackspace_api_tool/authentication.sh
+                                authenticate
+                        else
+                                echo "CHOOSE ONE OF THE AVAILABLE OPTIONS"
+                        fi
+                        ;;
+                2|INSTANCES)
+                        echo " PLEASE CHOOSE ONE OF THE FOLLOWING DATABASE INSTANCE OPTIONS: "
+                        if [ "~/rackspace_api_tool/instances.sh" ]; then
+                                source ~/rackspace_api_tool/instances.sh
+                                instances
+                        else
+                                echo "CHOOSE ONE OF THE AVAILABLE OPTIONS"
+                        fi
+                        ;;
+                3|EXIT)
+                        echo "THANK YOU FOR USING THE API CLIENT"
+                        exit
+                        ;;
+                *)
+                        echo "UNFORTUNATELY THIS IS NOT A VALID ENTRY. CLOSING. GOOD BYE"
+                        ;;
+        esac
+done
+
+}
---
+++ menu.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+function main_menu()
+{
+
+while true; do 
+	echo -e -n "\n\nCHOOSE ONE OF THE FOLLOWING OPTIONS:"
+	echo -e -n "\n1 MAIN MENU"
+	echo -e -n "\n2 DATABASES"
+	echo -e -\ "\n3 EXIT\n"
+
+	read CONFIRM
+	case $CONFIRM in
+		1|MAINMENU) 
+			echo " PLEASE AUTHORISE: "
+			if [ "~/rackspace_api_tool/main_menu.sh" ]; then
+				source ~/rackspace_api_tool/main_menu.sh
+				main_menu
+			else
+				echo "CHOOSE ONE OF THE AVAILABLE OPTIONS"
+			fi
+			;;
+		2|DATABASES)
+			echo " PLEASE CHOOSE ONE OF THE FOLLOWING DATABASE INSTANCE OPTIONS: "
+			if [ "~/rackspace_api_tool/instances.sh" ]; then
+				source ~/rackspace_api_tool/instances.sh
+				instances
+			else
+                                echo "CHOOSE ONE OF THE AVAILABLE OPTIONS"
+                        fi
+                        ;;
+		3|EXIT)
+			echo "THANK YOU FOR USING THE API CLIENT"
+			exit
+			;;
+		*) 
+			echo "UNFORTUNATELY THIS IS NOT A VALID ENTRY. CLOSING. GOOD BYE"
+			;;
+	esac
+done
+}
