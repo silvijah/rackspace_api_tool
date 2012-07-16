49ba3dd759d3d4033ea79a35809fa0a11963bee2
sylviaRS <silvia.mark@gmail.com>
2012-07-16 12:17:51 +0000

    edited

--- authentication.sh
+++ authentication.sh
@@ -2,9 +2,24 @@
 
 function authenticate()
 {
+
+authenticate= read -p "USERNAME " USERNAME; read -p "ACCOUNT " ACCOUNT; read -p "API KEY " APIKEY;read -p "DATACENTER LOCATION " LOCATION;
+
+auth_response="$(curl -i -s -XGET -H "X-Auth-User: $USERNAME" -H "X-Auth-Key: $APIKEY"  https://$LOCATION.identity.api.rackspacecloud.com/v1.0 | egrep -e '(^HTTP/1.1|^X-Auth-Token)')"
+
+APITOKEN=
+
+if [ "$(echo "$auth_response" | head -1 | awk '{print $2}')" = "204" ]; then
+        APITOKEN="$(echo "$auth_response" | tail -1 | awk '{print $NF}' | sed -e 's/[\r\n]//g')"
+else
+        echo    "Authentication failed"
+        exit
+fi
+
+
 #read -p "username " USERNAME
 #read -p "API key " APIKEY
 #read -p "ACCOUNT " ACCOUNT
 
-curl -i -XGET -H "X-Auth-User: $USERNAME" -H "X-Auth-Key: $APIKEY"  https://$LOCATION.identity.api.rackspacecloud.com/v1.0
+#curl -i -XGET -H "X-Auth-User: $USERNAME" -H "X-Auth-Key: $APIKEY"  https://$LOCATION.identity.api.rackspacecloud.com/v1.0
 } 
