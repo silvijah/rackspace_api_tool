73f733e59126612feaf8e6398b8936e5ea6cc884
sylviaRS <silvia.mark@gmail.com>
2012-07-17 20:06:27 +0000

    removed additional db options

--- create_db_existing_instance.sh
+++ create_db_existing_instance.sh
@@ -3,10 +3,8 @@
 function createdbexistinginstance() {
 
 read -p "Please Enter your New Database name " DB1
-read -p "Second DB server " DB2
 
 sed -i "6 s/\"name\":.*/\"name\": \"$DB1\"/" create_db_on_existing_instance
-sed -i "9 s/\"name\":.*/\"name\": \"$DB2\"/" create_db_on_existing_instance
 
 }
 
--- create_db_function.sh
+++ create_db_function.sh
@@ -3,7 +3,6 @@
 function createdbinstance() {
 
 read -p "Please Enter your New Database name " DB1
-read -p "Second DB server " DB2
 read -p "Please Enter your New Instance name " INSTANCE
 read -p "Please Enter your New Username " USER
 read -p "New Password " PASS
@@ -11,12 +10,11 @@ read -p "Specify your location LON/DFW/ORD " LOCATION
 read -p "Specify your Account Number " ACCOUNT
 
 sed -i "7 s/\"name\":.*/\"name\": \"$DB1\"/" createdbinstance
-sed -i "10 s/\"name\":.*/\"name\": \"$DB2\"/" createdbinstance
-sed -i "14 s/\"name\":.*/\"name\": \"$INSTANCE\",/" createdbinstance
-sed -i "19 s/\"name\":.*/\"name\": \"$DB1\"/" createdbinstance
-sed -i "22 s/\"name\":.*/\"name\": \"$USER\",/" createdbinstance
-sed -i "23 s/\"password\":.*/\"password\": \"$PASS\"/" createdbinstance
-sed -i "13 s/\"flavorRef\":.*/\"flaforRef\": \"https://$LOCATION.databases.api.rackspacecloud.com/v1.0/$ACCOUNT/flavors/1\",/" createdbinstance
+sed -i "11 s/\"name\":.*/\"name\": \"$INSTANCE\",/" createdbinstance
+sed -i "16 s/\"name\":.*/\"name\": \"$DB1\"/" createdbinstance
+sed -i "19 s/\"name\":.*/\"name\": \"$USER\",/" createdbinstance
+sed -i "20 s/\"password\":.*/\"password\": \"$PASS\"/" createdbinstance
+sed -i "10 s/\"flavorRef\":.*/\"flaforRef\": \"https://$LOCATION.databases.api.rackspacecloud.com/v1.0/$ACCOUNT/flavors/1\",/" createdbinstance
 
 }
 
