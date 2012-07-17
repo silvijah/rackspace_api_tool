c6bc59653a3b39cd61989efe972117b39ca5bfa7
sylviaRS <silvia.mark@gmail.com>
2012-07-17 21:17:38 +0000

    syntax error fixed

--- create_db_function.sh
+++ create_db_function.sh
@@ -14,7 +14,6 @@ sed -i "11 s/\"name\":.*/\"name\": \"$INSTANCE\",/" createdbinstance
 sed -i "16 s/\"name\":.*/\"name\": \"$DB1\"/" createdbinstance
 sed -i "19 s/\"name\":.*/\"name\": \"$USER\",/" createdbinstance
 sed -i "20 s/\"password\":.*/\"password\": \"$PASS\"/" createdbinstance
-sed -i "10 s/\"flavorRef\":.*/\"flaforRef\": \"https://$LOCATION.databases.api.rackspacecloud.com/v1.0/$ACCOUNT/flavors/1\",/" createdbinstance
-
+sed -i '10 s/"flavorRef":.*/"flavorRef": "https:\/\/$LOCATION.databases.api.rackspacecloud.com\/v1.0\/$ACCOUNT\/flavors\/1",/' createdbinstance
 }
 
